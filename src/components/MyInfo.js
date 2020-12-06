d7390d71084b8338234f15fdced169f3e2ade5b2
haoranyu <yu.haora@northeastern.edu>
2020-12-06 16:19:03 -0500

    seperate-components-folder

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Haoran Yu</h1>
+      <p>A MSCS student from Northeastern University</p>
+      <ul>
+        <li>Google intern</li>
+        <li>Facebook intern</li>
+        <li>Apple intern</li>
+      </ul>
+    </div>
+  )
+} 
+
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function MyInfo() {
-  return (
-    <div>
-      <h1>Haoran Yu</h1>
-      <p>A MSCS student from Northeastern University</p>
-      <ul>
-        <li>Google intern</li>
-        <li>Facebook intern</li>
-        <li>Apple intern</li>
-      </ul>
-    </div>
-  )
-}
+import MyInfo from "./components/MyInfo"
 
 ReactDOM.render(MyInfo(), document.getElementById("root"))
