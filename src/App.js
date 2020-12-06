425bb9495cafe4f6e9b773a93a98acfd4e95c587
haoranyu <yu.haora@northeastern.edu>
2020-12-06 18:47:35 -0500

    inline-styles-with-the-style-property

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react"
-import Header from "./components/Header"
-import "./App.css"
+import ReactDOM from "react-dom"
+
 function App() {
-  const firstName = "Haoran"
-  const lastName = "Yu"
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
+  const styles = {
+    color: "#FF8C00",
+    backgroundColor: "#FF2D00",
+    fontSize: "200px"
+  }
   return (
-    <div>
-      <Header />
-      <input type="checkbox" />
-      <p>Hello {firstName + " " + lastName}!</p>
-      <input type="checkbox" />
-      <p>Hello firstName + " " + lastName!</p>
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-    </div>
+    // <h1 style={{color: "#FF8C00", backgroundColor: "#FF2D00"}}>Good {timeOfDay}</h1>
+    <h1 style={styles}>Good {timeOfDay}</h1>
   )
 }
 
