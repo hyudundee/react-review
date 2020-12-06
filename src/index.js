61e2b2d4be30eab6dd91c2153bc6d9d932481d3e
haoranyu <yu.haora@northeastern.edu>
2020-12-06 16:51:10 -0500

    parent-child-component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import MyInfo from "./components/MyInfo"
-
-ReactDOM.render(MyInfo(), document.getElementById("root"))
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+ReactDOM.render(<App />, document.getElementById("root"))
