f1c3a0632124b65b673799c220eb2ee2fee8b930
haoranyu <yu.haora@northeastern.edu>
2020-12-06 18:24:11 -0500

    JSX-to-JavaScript-and-back

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react"
 import Header from "./components/Header"
 import "./App.css"
 function App() {
+  const firstName = "Haoran"
+  const lastName = "Yu"
   return (
     <div>
       <Header />
       <input type="checkbox" />
-      <p>Placeholder text here</p>
+      <p>Hello {firstName + " " + lastName}!</p>
       <input type="checkbox" />
-      <p>Placeholder text here</p>
+      <p>Hello firstName + " " + lastName!</p>
       <input type="checkbox" />
       <p>Placeholder text here</p>
       <input type="checkbox" />
